d54f0e119475b38cb2b0d85ab70e19d0b31c0cab
ub-victor <victoireushindi371@gmail.com>
2025-08-26 06:32:55 +0200

    Initial commit

--- js/details.js
+++ js/details.js
@@ -6,6 +6,7 @@
 // The result is assigned to the constant id.
 const id = new URLSearchParams(window.location.search).get('id');
 const container = document.querySelector('.details');
+const deleteBtn = document.querySelector('.delete');
 
 // let generate an endpoint URL for fetching the post details
 const renderDetails = async ()=>{
@@ -22,5 +23,12 @@ const renderDetails = async ()=>{
 
 
 }
+
+deleteBtn.addEventListener('click', async ()=>{
+    const res = await fetch(`http://localhost:3000/posts/${id}`,{ // Fetch the post with the given ID
+        method: 'DELETE'// deletes the post
+    });
+    window.location.replace('/index.html');// Redirect to the main page after deletion 
+})
 
 window.addEventListener('DOMContentLoaded', () => renderDetails());
